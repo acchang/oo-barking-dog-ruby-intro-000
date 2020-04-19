876a1bd81265b0930490c4267400f7586addc506
Andrew C <acchang4@gmail.com>
2020-04-19 01:19:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 class Dog
   # # Write a setter method, .name=, that allows you to give a dog a name.
-  #   def initialize(name)
-  #     @name = name
-  #   end
+    def initialize(name)
+      @name = name
+    end
   #
   #   def name=(name)
   #     @name = name
